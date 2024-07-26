49f5576cc567b05d615091e4b088d9aad26e5b5b
cesar pagliacci <pagliaccicesar@gmail.com>
2024-07-26 16:47:55 -0300

    se agrega cookies

--- main.js
+++ main.js
@@ -36,6 +36,35 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });*/
 
+//  cookies
+
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+dataLayer = [];
+
+if(!localStorage.getItem('cookies-aceptadas')){
+	avisoCookies.classList.add('activo');
+	fondoAvisoCookies.classList.add('activo');
+} else {
+	dataLayer.push({'event': 'cookies-aceptadas'});
+}
+
+botonAceptarCookies.addEventListener('click', () => {
+	avisoCookies.classList.remove('activo');
+	fondoAvisoCookies.classList.remove('activo');
+
+	localStorage.setItem('cookies-aceptadas', true);
+
+	dataLayer.push({'event': 'cookies-aceptadas'});
+});
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
   const buttons = document.querySelectorAll(".btn");
   const modalOverlay = document.querySelector(".modal-overlay");
