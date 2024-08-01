45052fa632bf987e9ef5882d93e4169f6ed53443
cesar pagliacci <pagliaccicesar@gmail.com>
2024-08-01 19:13:33 -0300

    se agrego boton rechazar cookies

--- main.js
+++ main.js
@@ -38,7 +38,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //  cookies
 
-const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+/*const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const botonRechazarCookies = document.getElementById('btn-rechazar-cookies');
 const avisoCookies = document.getElementById('aviso-cookies');
 const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
@@ -58,7 +59,46 @@ botonAceptarCookies.addEventListener('click', () => {
 	localStorage.setItem('cookies-aceptadas', true);
 
 	dataLayer.push({'event': 'cookies-aceptadas'});
+});*/
+
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const botonRechazarCookies = document.getElementById('btn-rechazar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+dataLayer = [];
+
+if (!localStorage.getItem('cookies-aceptadas')) {
+    avisoCookies.classList.add('activo');
+    fondoAvisoCookies.classList.add('activo');
+} else {
+    dataLayer.push({ 'event': 'cookies-aceptadas' });
+}
+
+botonAceptarCookies.addEventListener('click', () => {
+    avisoCookies.classList.remove('activo');
+    fondoAvisoCookies.classList.remove('activo');
+
+    localStorage.setItem('cookies-aceptadas', true);
+
+    dataLayer.push({ 'event': 'cookies-aceptadas' });
 });
+
+botonRechazarCookies.addEventListener('click', () => {
+    avisoCookies.classList.remove('activo');
+    fondoAvisoCookies.classList.remove('activo');
+
+    localStorage.removeItem('cookies-aceptadas');
+
+    // Opcional: puedes añadir un evento al dataLayer para indicar que se rechazaron las cookies
+    dataLayer.push({ 'event': 'cookies-rechazadas' });
+});
+
+
+
+
+
+  
 
 
 
